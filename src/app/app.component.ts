97ce7c2c62531c68c2b4fee14305d54c89a14082
cgraflund <connor.graf@gmail.com>
2024-03-06 23:57:51 -0700

    WIP webpage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
